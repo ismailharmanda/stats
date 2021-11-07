5e7003a81ffe1bc3327f7d0c14560a390cb1c94f
ismail harmanda <iharmanda@hotmail.com>
2021-11-07 23:52:17 +0300

    use enum(because it is gonna store very related values--no need to use object)

--- build/index.js
+++ build/index.js
@@ -10,16 +10,21 @@ var matches = fs_1.default
 })
     .split("\n")
     .map(function (item) { return item.split(","); });
-var homeWin = "H";
-var awayWin = "A";
-var draw = "D";
+// enum - enumeration
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult || (MatchResult = {}));
+var HomeWin = MatchResult.HomeWin, AwayWin = MatchResult.AwayWin, Draw = MatchResult.Draw;
 var manUnitedWins = 0;
 for (var _i = 0, matches_1 = matches; _i < matches_1.length; _i++) {
     var match = matches_1[_i];
-    if (match[1] === "Man United" && match[5] === homeWin) {
+    if (match[1] === "Man United" && match[5] === HomeWin) {
         manUnitedWins++;
     }
-    else if (match[2] === "Man United" && match[5] === awayWin) {
+    else if (match[2] === "Man United" && match[5] === AwayWin) {
         manUnitedWins++;
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -7,16 +7,20 @@ const matches = fs
   .split("\n")
   .map((item: string): string[] => item.split(","));
 
-const homeWin = "H";
-const awayWin = "A";
-const draw = "D";
+// enum - enumeration
+enum MatchResult {
+  HomeWin = "H",
+  AwayWin = "A",
+  Draw = "D",
+}
+const { HomeWin, AwayWin, Draw } = MatchResult;
 
 let manUnitedWins = 0;
 
 for (let match of matches) {
-  if (match[1] === "Man United" && match[5] === homeWin) {
+  if (match[1] === "Man United" && match[5] === HomeWin) {
     manUnitedWins++;
-  } else if (match[2] === "Man United" && match[5] === awayWin) {
+  } else if (match[2] === "Man United" && match[5] === AwayWin) {
     manUnitedWins++;
   }
 }
