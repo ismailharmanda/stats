5add0b19a07912bf19dc64a3d59b4c9101f92f07
ismail harmanda <iharmanda@hotmail.com>
2021-11-09 00:19:17 +0300

    creat a new MatchReader interface for interface based approach

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,10 +1,14 @@
 import fs from "fs";
 
-export abstract class CsvFileReader<T> {
-  data: T[] = [];
+import { dateStringToDate } from "./utils";
+import { MatchResult } from "./MathResult";
+
+type MatchData = [Date, string, string, number, number, MatchResult, string];
+
+export class CsvFileReader {
+  data: MatchData[] = [];
 
   constructor(public fileName: string) {}
-  abstract mapRow(row: string[]): T;
 
   read(): void {
     this.data = fs
@@ -13,6 +17,16 @@ export abstract class CsvFileReader<T> {
       })
       .split("\n")
       .map((row: string): string[] => row.split(","))
-      .map(this.mapRow);
+      .map((row: string[]): MatchData => {
+        return [
+          dateStringToDate(row[0]),
+          row[1],
+          row[2],
+          parseInt(row[3]),
+          parseInt(row[4]),
+          row[5] as MatchResult,
+          row[6],
+        ];
+      });
   }
 }
