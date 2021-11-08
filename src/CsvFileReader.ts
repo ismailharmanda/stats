6a986e71726f7c4b3db7d15e7f0807cd465342c6
ismail harmanda <iharmanda@hotmail.com>
2021-11-09 00:45:23 +0300

    transform data

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,12 +1,7 @@
 import fs from "fs";
 
-import { dateStringToDate } from "./utils";
-import { MatchResult } from "./MathResult";
-
-type MatchData = [Date, string, string, number, number, MatchResult, string];
-
 export class CsvFileReader {
-  data: MatchData[] = [];
+  data: string[][] = [];
 
   constructor(public fileName: string) {}
 
@@ -16,17 +11,6 @@ export class CsvFileReader {
         encoding: "utf-8",
       })
       .split("\n")
-      .map((row: string): string[] => row.split(","))
-      .map((row: string[]): MatchData => {
-        return [
-          dateStringToDate(row[0]),
-          row[1],
-          row[2],
-          parseInt(row[3]),
-          parseInt(row[4]),
-          row[5] as MatchResult,
-          row[6],
-        ];
-      });
+      .map((row: string): string[] => row.split(","));
   }
 }
