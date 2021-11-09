2682090dcd119c5fc3500a90ba8e71a31d09e1bf
ismail harmanda <iharmanda@hotmail.com>
2021-11-09 13:28:24 +0300

    use static class methods for a cleaner and easier method calling

--- src/MatchReader.ts
+++ src/MatchReader.ts
@@ -1,6 +1,7 @@
 import { dateStringToDate } from "./utils";
 import { MatchResult } from "./MathResult";
 import { MatchData } from "./MatchData";
+import { CsvFileReader } from "./CsvFileReader";
 
 interface DataReader {
   read(): void;
@@ -8,6 +9,10 @@ interface DataReader {
 }
 
 export class MatchReader {
+  static fromCsv(fileName: string): MatchReader {
+    return new MatchReader(new CsvFileReader(fileName));
+  }
+
   matches: MatchData[] = [];
   constructor(public reader: DataReader) {}
 
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,9 @@
 import { MatchReader } from "./MatchReader";
-import { CsvFileReader } from "./CsvFileReader";
-import { ConsoleReport } from "./reportTargets/ConsoleReport";
-import { WinsAnalysis } from "./analyzers/WinsAanalysis";
+
 import { Summary } from "./Summary";
-import { HtmlReport } from "./reportTargets/HtmlReport";
 
-// Create an object that satisfies the  "DataReader" interface
-const csvFileReader = new CsvFileReader("football.csv");
-
-// Create an instance of MatchReader and pass in something satisfying
-// the "DataReader" interface
-
-const matchReader = new MatchReader(csvFileReader);
+const matchReader = MatchReader.fromCsv("football.csv");
 matchReader.load();
 
-// const summary = new Summary(
-//   new WinsAnalysis("Man United"),
-//   new ConsoleReport()
-// );
-const summary = new Summary(new WinsAnalysis("Man United"), new HtmlReport());
-
+const summary = Summary.winsAnalysisWithHtmlReport("Man United");
 summary.buildAndPrintReport(matchReader.matches);
